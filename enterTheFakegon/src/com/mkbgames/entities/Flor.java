c00a5a8c00e7e59db61460532df47e568dd0fb0a
Eryck Torres <108304916+EryckKawa@users.noreply.github.com>
2023-05-03 21:29:10 -0300

    Delete Flor.java

--- enterTheFakegon/src/com/mkbgames/entities/Flor.java
+++
@@ -1,9 +0,0 @@
-package com.gcstudios.entities;
-
-import java.awt.image.BufferedImage;
-
-public class Flor extends Entity{
-	public Flor(int x, int y, int width, int height, BufferedImage sprite) {
-		super(x, y, width, height, sprite);
-	}
-}
